a51def700465b09b4cdc443a0b8345f325381ff1
Howie <ellla@ad.unc.edu>
2023-07-10 12:03:16 -0400

    final code of part 1 to frass analysis

--- frass_image_data.r
+++ frass_image_data.r
@@ -6,9 +6,10 @@ library(tidyverse)
 library(readr)
 library(data.table)
 library(dplyr)
+library(lubridate)
 
 # working directory issue - manually set
-setwd("z:/Databases/CaterpillarsCount/Frass/2023/Results/")
+setwd("//ad.unc.edu/bio/HurlbertLab/Databases/CaterpillarsCount/Frass/2023/Results")
 
 filelist = list.files()
 
@@ -16,36 +17,85 @@ list_of_files <- list.files(path = ".", recursive = TRUE,
                             pattern = "\\.txt$", 
                             full.names = TRUE)
 
-DT <- rbindlist(sapply(list_of_files, fread, simplify = FALSE),
+df1 <- rbindlist(sapply(list_of_files, fread, simplify = FALSE),
                 use.names = TRUE, idcol = "FileName", fill = TRUE)
 
+
+# For loop, read in each file
+
+output = data.frame(Site = NULL, Trap = NULL, Date = NULL, X = NULL, Area = NULL)
+
+for (file in filelist) {
+  
+  tmpfile = read.table(file, sep = '\t', header = T)
+  
+  # extracting site, date, and trap from the filename using word()
+  # example:
+  
+  Datestring <- word(file, sep = "_", 1) 
+  Site <- word(file, sep = "_", 2) 
+  Trap <- word(file, sep = "_", 3) %>% word(sep = "\\.", 1)
+  
+  Date = paste(substr(Datestring, 1, 4), substr(Datestring, 5, 6), substr(Datestring, 7, 8), sep = "-")
+  
+  tmpdf = data.frame(Site = NULL, Trap = NULL, Date = NULL, X = NULL, Area = NULL)
+  
+   tmpdf = tmpfile[, c("X", "Area")]
+  tmpdf$Date = rep(Date, nrow(tmpdf))
+  tmpdf$Site = rep(Site, nrow(tmpdf))
+  tmpdf$Trap = rep(Trap, nrow(tmpdf))
+  
+  output = rbind(output, tmpdf)
+  
+} # end loop
+
+output = output[, c("Site", "Trap", "Date", "X", "Area")]
+output$Date = as.Date(output$Date, format = "%Y-%m-%d")
+names(output)[4] = "Particle"
+
+
+
+
+
+
+
+
+
+
 # delete columns
-DT = subset(DT, select = -c(7:11) )
+df1 = subset(df1, select = -c(7:11) )
 
 # rename columns 
-colnames(DT)[2] ="Number of Frass"
+colnames(df1)[2] ="Number of Frass"
 
 # create site and date columns by splicing name
 # new dataframe for only the file name
-list_of_names <- data.table(DT$FileName)
+list_of_names <- data.table(df1$FileName)
 
 # extracting site, date, and trap from the filename using word()
 # example:
 
-Date <- word(list_of_names, sep = "_", 1) 
-Site <- word(list_of_names, sep = "_", 2) 
-Trap <- word(list_of_names, sep = "_", 3)
+Date1 <- word(list_of_names, sep = "_", 1) 
+Site1 <- word(list_of_names, sep = "_", 2) 
+Trap1 <- word(list_of_names, sep = "_", 3)
 
 # subset Trap and Date by removing unnecessary characters
+# loop it for all files
+length <- row(list_of_names)
 
-Date2 <- word(Date, sep = "/", 2)
 
-Trap2 <- word(Trap, sep = ".txt", 1)
+for(i in 1:ncol(list_of_names)) {                                             # Loop over character vector
+    Date <- word(Date1, sep = "/", 2)
+  }  
+  
+for(i in 1:ncol(list_of_names)) {                                             # Loop over character vector
+    Trap <- word(Trap1, sep = ".txt", 1)
+  }    
 
-# store into DT by adding new columns 
+# store into df1 (dataframe 1) by adding new columns 
 
-df2 <- cbind(DT, Date2, Site, Trap2)
-df2
+df2 <- cbind(df1, Date, Site, Trap)
+
 
 
 
