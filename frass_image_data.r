22bcd5d0829332af109abcba2774d3d3c24d2211
Howie <ellla@ad.unc.edu>
2023-07-19 10:40:43 -0400

    error

--- frass_image_data.r
+++ frass_image_data.r
@@ -8,7 +8,7 @@ library(data.table)
 library(dplyr)
 
 # working directory issue - manually set
-setwd("z:/Databases/CaterpillarsCount/Frass/2023/Results/")
+setwd("C://ad.unc.edu/bio/HurlbertLab/Databases/CaterpillarsCount/Frass/2023/Results")
 
 filelist = list.files()
 
@@ -16,18 +16,18 @@ list_of_files <- list.files(path = ".", recursive = TRUE,
                             pattern = "\\.txt$", 
                             full.names = TRUE)
 
-DT <- rbindlist(sapply(list_of_files, fread, simplify = FALSE),
+df1 <- rbindlist(sapply(list_of_files, fread, simplify = FALSE),
                 use.names = TRUE, idcol = "FileName", fill = TRUE)
 
 # delete columns
-DT = subset(DT, select = -c(7:11) )
+df1 = subset(DT, select = -c(7:11) )
 
 # rename columns 
 colnames(DT)[2] ="Number of Frass"
 
 # create site and date columns by splicing name
 # new dataframe for only the file name
-list_of_names <- data.table(DT$FileName)
+list_of_names <- data.table(df1$FileName)
 
 # extracting site, date, and trap from the filename using word()
 # example:
@@ -37,15 +37,20 @@ Site <- word(list_of_names, sep = "_", 2)
 Trap <- word(list_of_names, sep = "_", 3)
 
 # subset Trap and Date by removing unnecessary characters
+# loop it for all files
+length <- row(list_of_names)
 
-Date2 <- word(Date, sep = "/", 2)
+for i in length
+  Date2 <- word(Date, sep = "/", 2)
 
-Trap2 <- word(Trap, sep = ".txt", 1)
+for i in length
+  Trap2 <- word(Trap, sep = ".txt", 1)
 
-# store into DT by adding new columns 
 
-df2 <- cbind(DT, Date2, Site, Trap2)
-df2
+# store into df1 (dataframe 1) by adding new columns 
+
+df2 <- cbind(df1, Date2, Site, Trap2)
+
 
 
 
